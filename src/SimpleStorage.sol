716c74b9c5403ad2356456b0ca9637f43a717dd1
Suraj Yakkha <yakkhasuraj@gmail.com>
2024-08-28 22:48:41 +0545

    feat: migrate to foundry

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
-
-// Author: @yakkhasuraj
 contract SimpleStorage {
     uint256 favoriteNumber;
 
